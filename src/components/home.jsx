a1d15aaf76ecc3f5f6c3d7612e2ea31cd34d4550
Angela Francisco <angelat.francisco@gmail.com>
2022-06-10 11:17:13 -0700

    updating content text style

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,10 +3,10 @@ const Home = () => {
         <div className="content-container home">
             <div className="hero-wrapper home">
                 <div className="hero-box-text">
-                    <p className="hero-text-small">&#128075;&#127997; Hi there, I'm</p>
-                    <h1 className="hero-text-name">Angela Francisco</h1>
-                    <h3 className="hero-text-title">Software Engineer</h3>
-                    <p className="hero-text-small">&#128205; Portland, OR</p>
+                    <p className="hero-text small">&#128075;&#127997; Hi there, I'm</p>
+                    <h1 className="hero-text name">Angela<br/>Francisco</h1>
+                    <h3 className="hero-text title">Software Engineer</h3>
+                    <p className="hero-text small">&#128205; Portland, OR</p>
                     <div className="hero-box-contact">
                         <a href="#connect" className="hero-link">Let's connect!</a>
                     </div>
