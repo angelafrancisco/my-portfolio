a77c77414189bb939f1ece7c2384f2e427fa1d41
Angela Francisco <angelat.francisco@gmail.com>
2022-06-29 18:11:25 -0700

    updating contact layout

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -5,25 +5,27 @@ const Contact = () => {
                 <h1 className="section-title">Contact</h1>
                 <div className="section-box contact">
                     <p className="section-text">
-                        Let's connect! I'd love to grab a coffee and chat either in person or virtually!
+                        Let's connect!
+                        I love learning about individual experiences within tech! 
+                        And a bonus, if you're in the Portland area, I'd love to grab coffee too!
                     </p>
-                    <ul className="section-text">
-                        <li>
+                    <div className="contact-links">
+                        <div className="contact-links-box">
                             <img src="#" alt="LinkedIn" />
                             <span> LinkedIn: </span>
                             <a href="https://www.linkedin.com/in/angela-francisco/" className="contact-link">@angela-francisco</a>
-                        </li>
-                        <li>
+                        </div>
+                        <div className="contact-links-box">
                             <img src="#" alt="Email" />
                             <span> Email: </span>
                             <a href="mailto:angelat.francisco@gmail.com?subject=Hello!" className="contact-link">angelat.francisco@gmail.com</a>
-                        </li>
-                        <li>
+                        </div>
+                        <div className="contact-links-box">
                             <img src="#" alt="GitHub" />
                             <span> GitHub: </span>
                             <a href="https://github.com/angelafrancisco" className="contact-link">@angelafrancisco</a>
-                        </li>
-                    </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
